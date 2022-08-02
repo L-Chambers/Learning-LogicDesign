4b5a4965585eda6890b94d6ad14d745e5af8a124
Lauren Chambers <43095590+L-Chambers@users.noreply.github.com>
2022-08-02 15:16:37 -0700

    structural mux, first commit

---
+++ Fundamentals/Multiplexers/structural.v
@@ -0,0 +1,17 @@
+
+//
+// MULTIPLEX 1 bit out of 4 bits
+//
+//
+module mux_4to1(
+  input [3:0] in,   // Input Set @in: choose from 4 bits
+  input [1:0] sel,  // Select @sel: 2^2 = 4 possible values
+  output o          // Output @o: single bit from @in
+);
+
+  assign o = (~sel[1] & ~sel[0] & in[0]) | // 2'b00 selects bit 0
+             (~sel[1] &  sel[0] & in[1]) | // 2'b01 selects bit 1
+             ( sel[1] & ~sel[0] & in[2]) | // 2'b10 selects bit 2
+             ( sel[1] &  sel[0] & in[3]);  // 2'b11 selects bit 3
+
+endmodule
